04e2fcba34d2326c54ce99b7e04d6e5556595814
Patrick Armstrong <pxa3203@hotmail.com>
2021-08-05 20:16:36 +1000

    optimized for mobile by adding hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,8 +13,23 @@ function showContact() {
 
 }
 
-
 function hideContact() {
     document.getElementById("US-Contact").style.display = "none";
     document.getElementById("AU-Contact").style.display = "none";
+}
+
+function iconRight() {
+    document.getElementById("right-col").style.display = "none";
+    document.getElementById("left-col").style.width = "100%";
+    document.getElementById("left-col").style.display = "inline";
+    document.getElementById("left-icon").style.display = "inline";
+    document.getElementById("left-icon").style.float = "left";
+    document.getElementById("left-icon").style.margin = "3%";
+}
+
+function iconLeft() {
+    document.getElementById("left-icon").style.display = "none";
+    document.getElementById("left-col").style.display = "none";
+    document.getElementById("right-col").style.display = "inline";
+    document.getElementById("right-col").style.width = "100%";
 }
